9e6e01008bb0a8ae36ad321de0c17b6ef137d920
Jorge Álamo <jorgealamosan@gmail.com>
2024-04-02 13:57:04 +0100

    Publication y Saved (sin acabar)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import {FooterComponent} from "./footer/footer.component";
 import {AccountComponent} from "./account/account.component";
 import {AddPublicationComponent} from "./add-publication/add-publication.component";
 import {EditProfileComponent} from "./edit-profile/edit-profile.component";
+import {PublicationComponent} from "./publication/publication.component";
+import {SavedComponent} from "./saved/saved.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, AccountComponent, AddPublicationComponent, EditProfileComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AccountComponent, AddPublicationComponent, EditProfileComponent, PublicationComponent, SavedComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
