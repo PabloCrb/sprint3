924107341c769997de0b769fa33d7dc0ea61d90c
Jorge Álamo <jorgealamosan@gmail.com>
2024-04-08 10:28:23 +0100

    edit profile responsive

--- src/app/edit-profile-page/edit-profile-page.component.ts
+++ src/app/edit-profile-page/edit-profile-page.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
 import {FooterComponent} from "../footer/footer.component";
 import {EditProfileComponent} from "../edit-profile/edit-profile.component";
+import {PhoneHeaderComponent} from "../phone-header/phone-header.component";
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-edit-profile-page',
@@ -9,11 +12,22 @@ import {EditProfileComponent} from "../edit-profile/edit-profile.component";
   imports: [
     HeaderComponent,
     FooterComponent,
-    EditProfileComponent
+    EditProfileComponent,
+    PhoneHeaderComponent,
+    NgClass,
+    NgIf
   ],
   templateUrl: './edit-profile-page.component.html',
   styleUrl: './edit-profile-page.component.css'
 })
 export class EditProfilePageComponent {
+  isPhoneScreen = false;
 
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([
+      `(max-width: 720px)`
+    ]).subscribe(result => {
+      this.isPhoneScreen = result.matches;
+    });
+  }
 }
