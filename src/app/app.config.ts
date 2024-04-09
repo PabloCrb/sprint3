297d20a1ce2196e49f9f750066c68a14eb517c96
Pablo <zerytv75@gmail.com>
2024-04-09 13:53:20 +0100

    Imaginate que funciona firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"prueba-53b0b","appId":"1:542323782591:web:87384f90a4ebb9607a670a","storageBucket":"prueba-53b0b.appspot.com","apiKey":"AIzaSyBYXUQsPKAJUTi6n8i4evd3sPGcw1zZkso","authDomain":"prueba-53b0b.firebaseapp.com","messagingSenderId":"542323782591","measurementId":"G-2KVJ43HM5T"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase())), importProvidersFrom(provideFunctions(() => getFunctions())), importProvidersFrom(provideStorage(() => getStorage())), importProvidersFrom(provideRemoteConfig(() => getRemoteConfig()))]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBYXUQsPKAJUTi6n8i4evd3sPGcw1zZkso",
+    authDomain: "prueba-53b0b.firebaseapp.com",
+    projectId: "prueba-53b0b",
+    storageBucket: "prueba-53b0b.appspot.com",
+    messagingSenderId: "542323782591",
+    appId: "1:542323782591:web:87384f90a4ebb9607a670a",
+    measurementId: "G-2KVJ43HM5T"
+  }
+};
